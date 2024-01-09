049e12ef415abec72bdd038b03206bb109f82dbb
Bigi-Med <medbigi2000@gmail.com>
2024-01-09 16:21:50 +0100

    MBI: refactorng handle response

--- src/bounce.js
+++ src/bounce.js
@@ -9,6 +9,9 @@ const url = queryData['url'] + '?' + queryString.stringify(queryParams)
 const parsedUrl = new URL(url)
 const method = queryData['method']
 const headers = queryData['header']
+const MAX_REDIRECTS = queryData['max_redirects']?queryData[max_redirects]:3
+const followRedirects = queryData['followRedirects']?queryData['followRedirects']:false 
+
 
 const options = {
   hostname :parsedUrl.hostname,
@@ -18,25 +21,30 @@ const options = {
   headers : headers
 }
 
-const request = http.request(options, (res)=> {
-    let data = '';
+function handleResponse(res,data){
+    try{
+      const parsedData = JSON.parse(data)
+      console.log(parsedData)
+    } catch (e){
+      console.log("Data was received in a non-JSON format, printing data as it is ....")
+      console.log(data);
+    }
+}
 
-    res.on('data',(chunk)=> data+=chunk)
-    res.on('end',()=>{
-      try{
-        const parsedData = JSON.parse(data)
-        console.log(parsedData)
-      } catch (e){
-        console.log("Data was received in a non-JSON format, printing data as it is ....")
-        console.log(data);
-      }
-    });
-  })
+const request = http.request(options, (res)=> {
+  let data = '';
+
+  res.on('data',(chunk)=> data+=chunk)
+  res.on('end',()=>handleResponse(res,data));
+})
+
 request.setTimeout(queryData.timeout,() => {
   request.abort();
   console.log("Request timed out")
 })
+
 request.on("error", (err)=> console.log(`Problem with the request : ${err.message}`))
+
 request.end()
 
 
